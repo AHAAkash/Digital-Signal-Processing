b7ffab8b3180ee8261fa7a159aaa5a9d26317fd5
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2024-11-04 12:21:27 +0600

    Create Write a program to implement the sum of two array and display it.m

---
+++ Write a program to implement the sum of two array and display it.m
@@ -0,0 +1,14 @@
+%Write a program to implement the sum of two array and display it
+clc;
+clear all;
+close all;
+
+a=[1 2 3 4];
+b=[4 5 6 7];
+sum=a+b;  % sum of two array
+disp(sum);   % display summation
+c=a;   %Assign array a to c
+k=5;
+summ=a+k;
+disp(summ);
+plot(a);
