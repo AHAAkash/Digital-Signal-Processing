ec6407658444647fa0627bc906ba3dc659e1cce4
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2024-11-04 12:42:22 +0600

    Create Write a program to learn input and output format in MATLAB.m

---
+++ Write a program to learn input and output format in MATLAB.m
@@ -0,0 +1,11 @@
+%Write a program to learn input and output format in MATLAB
+clc;
+clear all;
+close all;
+
+a=input('Enter first number');  % Input the number
+b=input('Enter second number');  % Input the number
+
+sum=a+b;  %Summation
+
+fprintf("sum=%d",sum);   %Display output into command window
